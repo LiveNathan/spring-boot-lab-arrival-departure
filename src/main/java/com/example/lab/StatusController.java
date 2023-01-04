24673f95b0e6afa862fa461f4c798e97a257b3c6
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-01-04 12:51:21 -0600

    first commit

---
+++ src/main/java/com/example/lab/StatusController.java
@@ -0,0 +1,25 @@
+package com.example.lab;
+
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Data
+public class StatusController {
+    private final StatusService statusService;
+
+    @GetMapping("/arrival")
+    public String sayHello() {
+        return "springfundamentals/hello";
+    }
+
+    @GetMapping("/departure")
+    public String sayGoodbye() {
+        return "springfundamentals/goodbye";
+    }
+    @GetMapping("/success")
+    public String saySuccess() {
+        return "springfundamentals/success";
+    }
+}
